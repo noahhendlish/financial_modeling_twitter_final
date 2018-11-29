7fc5f615e9d3865820c5dc57aa1a0d72a2f4713f
Noah Hendlish <nhendlis@tulane.edu>
2018-11-28 18:46:02 -0600

    only analyzing ".._sig"--> signif variables

--- TwitterMaliciousTweetsAnalysis.R
+++ TwitterMaliciousTweetsAnalysis.R
@@ -55,10 +55,10 @@ all_tweets$tweetid  <-  as.numeric(all_tweets$tweetid )
 all_tweets$follower_count <-  as.numeric(all_tweets$follower_count)
 all_tweets$following_count <-  as.numeric(all_tweets$following_count)
 all_tweets$account_creation_date <-  as.Date(all_tweets$account_creation_date)
-all_tweets$tweet_date <-  as.Date(all_tweets$tweet_date )
-all_tweets$is_reply <-  as.numeric(all_tweets$is_reply )
+all_tweets$tweet_date <-  as.Date(all_tweets$tweet_date)
+all_tweets$is_reply <-  as.numeric(all_tweets$is_reply)
 all_tweets$is_quote <-  as.numeric(all_tweets$is_quote)
-all_tweets$is_Retweet <-  as.numeric(all_tweets$is_Retweet )
+all_tweets$is_Retweet <-  as.numeric(all_tweets$is_Retweet)
 all_tweets$retweet_count <-  as.numeric(all_tweets$retweet_count)
 all_tweets$like_count <-  as.numeric(all_tweets$like_count)
 all_tweets$user_has_reported_location <-  as.numeric(all_tweets$user_has_reported_location)
@@ -144,27 +144,27 @@ all_tweets$tweet_client <-as.factor(ifelse(all_tweets$tweet_client=='Twitter Web
 #split data sets into qualitative and quantitative
 quanti_headers = c("follower_count" ,"following_count",
 									 "is_reply",  "is_quote","is_Retweet", "retweet_count", "like_count","user_has_reported_location", 
-									 "acct_tweet_lang_same", "tweet_lang_english","tweet_lang_russian", "has_hashtags","num_hashtags",
-									 "has_urls", "num_urls", "has_user_mentions", "num_user_mentions",            
-									 "diff_tweet_acct_creation_time", "malicious")
+									 "acct_tweet_lang_same", "tweet_lang_english","tweet_lang_russian","num_hashtags",
+									 "num_urls", "num_user_mentions",            
+									 "diff_tweet_acct_creation", "malicious")
 quali_headers = c("account_language", "tweet_language", "tweet_client")
 all_final_headers =c("account_language", "tweet_language", "tweet_client","follower_count" ,"following_count",
 										 "is_reply",  "is_quote","is_Retweet", "retweet_count", "like_count","user_has_reported_location", 
-										 "acct_tweet_lang_same", "tweet_lang_english","tweet_lang_russian", "has_hashtags","num_hashtags",
-										 "has_urls", "num_urls", "has_user_mentions", "num_user_mentions",            
-										 "diff_tweet_acct_creation_time", "malicious")
+										 "acct_tweet_lang_same", "tweet_lang_english","tweet_lang_russian","num_hashtags",
+										 "num_urls", "num_user_mentions",            
+										 "diff_tweet_acct_creation", "malicious")
 all_tweets_quanti <- data.frame(all_tweets$follower_count, all_tweets$following_count, 
 														all_tweets$is_reply,  all_tweets$is_quote, all_tweets$is_Retweet, all_tweets$retweet_count, 
 														all_tweets$like_count, all_tweets$user_has_reported_location, all_tweets$acct_tweet_lang_same,         
-														all_tweets$tweet_lang_english,all_tweets$tweet_lang_russian, all_tweets$has_hashtags,
-														all_tweets$num_hashtags, all_tweets$has_urls, all_tweets$num_urls, all_tweets$has_user_mentions, 
+														all_tweets$tweet_lang_english,all_tweets$tweet_lang_russian,
+														all_tweets$num_hashtags, all_tweets$num_urls, 
 														all_tweets$num_user_mentions,all_tweets$diff_tweet_acct_creation_time, all_tweets$malicious)
 all_tweets_quali <- data.frame(all_tweets$account_language, all_tweets$tweet_language, all_tweets$tweet_client)
 all_tweets_final <- data.frame(all_tweets$account_language, all_tweets$tweet_language, all_tweets$tweet_client, all_tweets$follower_count, all_tweets$following_count, 
 																all_tweets$is_reply,  all_tweets$is_quote, all_tweets$is_Retweet, all_tweets$retweet_count, 
 																all_tweets$like_count, all_tweets$user_has_reported_location, all_tweets$acct_tweet_lang_same,         
-																all_tweets$tweet_lang_english,all_tweets$tweet_lang_russian, all_tweets$has_hashtags,
-																all_tweets$num_hashtags, all_tweets$has_urls, all_tweets$num_urls, all_tweets$has_user_mentions, 
+																all_tweets$tweet_lang_english,all_tweets$tweet_lang_russian,
+																all_tweets$num_hashtags, all_tweets$num_urls, 
 																all_tweets$num_user_mentions,all_tweets$diff_tweet_acct_creation_time, all_tweets$malicious) 
 colnames(all_tweets_quali) <-quali_headers
 colnames(all_tweets_quanti) <- quanti_headers
@@ -172,7 +172,13 @@ colnames(all_tweets_final) <- all_final_headers
 non_malicious_in_total_set <- subset(x=all_tweets_final, all_tweets_final$malicious ==0)
 malicious_in_total_set <- subset(x=all_tweets_final, all_tweets_final$malicious == 1)
 percent_of_malicious_in_total_set= nrow(malicious_in_total_set)/nrow(all_tweets_final)
-
+sapply(all_tweets_final,function(x) sum(x == 'international/unkown'))
+sapply(all_tweets_final,function(x) sum(x == 'undefined'))
+sapply(all_tweets_final,function(x) sum(x == 'other'))
+sapply(all_tweets_final,function(x) sum(x == 'Other'))
+sapply(all_tweets_final, function(x) length(unique(x)))
+#ONLY TAKE SIGNIFICGANT VARIABLES (COLUMNS)
+all_tweets_sig = all_tweets_final[c(1:nrow(all_tweets_final)),c(2,3,4,5,6,7,8,9,10,11,12,15,16,17,18,19)]
 ######### Sampling  ##################### Sampling  ##################### Sampling  ############
 #Random Sampling (Train and Test)
 #We may split the data (given population) into random samples with 50-50, 60-40 or 70-30 ratios
@@ -187,12 +193,14 @@ percent_of_malicious_in_set= nrow(malicious_in_set)/nrow(all_tweets_final)
 div_part <- sort(sample(nrow(all_tweets_final), nrow(all_tweets_final)*.5))
 #select training sample 
 train<-all_tweets_final[div_part,] # 70% here
+train_sig <- all_tweets_final[div_part,c(2,3,4,5,6,7,8,9,10,11,12,15,16,17,18,19)]
 colnames(train)<- all_final_headers
 non_malicious_in_train <- subset(x=train, train$malicious ==0)
 malicious_in_train <- subset(x=train, train$malicious ==1)
 percent_of_malicious_in_train = nrow(malicious_in_train)/nrow(train)
 # put remaining into test sample
 test<-all_tweets_final[-div_part,] # rest of the 30% data goes here
+test_sig <- all_tweets_final[-div_part,c(2,3,4,5,6,7,8,9,10,11,12,15,16,17,18,19)]
 non_malicious_in_test<- subset(x=train, train$malicious ==0)
 colnames(test)<- all_final_headers
 malicious_in_test <- subset(x=train, train$malicious ==1)
@@ -205,10 +213,13 @@ help(glm)
 regression1 <-glm(malicious~.,data=train,family=binomial())
 regression2 <- glm(malicious~  tweet_client+follower_count+following_count+is_reply+is_quote +is_Retweet +retweet_count+like_count+user_has_reported_location+acct_tweet_lang_same+tweet_lang_english+tweet_lang_russian+num_hashtags+ num_urls+num_user_mentions+diff_tweet_acct_creation_time,data=train,family=binomial())
 regression3 <- glm(malicious~  tweet_client+follower_count+following_count+is_reply+is_quote +is_Retweet +retweet_count+like_count+user_has_reported_location+acct_tweet_lang_same+tweet_language+num_hashtags+ num_urls+num_user_mentions+diff_tweet_acct_creation_time,data=train,family=binomial())
-
+regression4 <- glm(malicious~  follower_count+following_count+is_reply+is_quote +is_Retweet +retweet_count+like_count+user_has_reported_location+acct_tweet_lang_same+tweet_lang_english+tweet_lang_russian+num_hashtags+ num_urls+num_user_mentions+diff_tweet_acct_creation_time,data=train,family=binomial())
+regression_sig <-glm(malicious~.,data=train_sig,family=binomial())
 summary(regression1)
 summary(regression2)
 summary(regression3)
+summary(regression4)
+summary(regression_sig)
 
 # List of significant variables and features with p-value <0.01
 significant.variables <- summary(regression3)$coeff[-1,4] < 0.01
@@ -217,28 +228,118 @@ prob <- predict(regression3, type = "response")
 res <- residuals(regression3, type = "deviance")
 
 ## CIs using profiled log-likelihood
-confint(regression3)
-
+CI_profile_log_likelihood = confint(regression3)
+summary(CI_profile_log_likelihood)
 ## CIs using standard errors
-confint.default(regression3)
+CI_using_stderrors = confint.default(regression3)
+summary(CI_using_stderrors)
 ## odds ratios and 95% CI
-exp(cbind(OR = coef(regression3), confint(regression3)))
-
+odd_ratios_95CI = exp(cbind(OR = coef(regression3), confint(regression3)))
+summary(odd_ratios_95CI)
 #score test data set
-#test$m1_score <- predict(regression3,type='response',test)
-#m1_pred <- prediction(test$m1_score, test$good_bad_21)
-#m1_perf <- performance(m1_pred,"tpr","fpr")
+test$m1_score <- predict(regression3,type='response',test)
+m1_pred <- prediction(test$m1_score, test$good_bad_21)
+m1_perf <- performance(m1_pred,"tpr","fpr")
 
 #ROC
-#par(mfrow=c(1,1))
-#plot(m1_perf, lwd=2, colorize=TRUE, main="ROC m1: Logistic Regression Performance")
-#lines(x=c(0, 1), y=c(0, 1), col="red", lwd=1, lty=3);
-#lines(x=c(1, 0), y=c(0, 1), col="green", lwd=1, lty=4)
+par(mfrow=c(1,1))
+plot(m1_perf, lwd=2, colorize=TRUE, main="ROC m1: Logistic Regression Performance")
+lines(x=c(0, 1), y=c(0, 1), col="red", lwd=1, lty=3);
+lines(x=c(1, 0), y=c(0, 1), col="green", lwd=1, lty=4)
 
 # Plot precision/recall curve
-#m1_perf_precision <- performance(m1_pred, measure = "prec", x.measure = "rec")
+m1_perf_precision <- performance(m1_pred, measure = "prec", x.measure = "rec")
 
 plot(m1_perf_precision, main="m1 Logistic:Precision/recall curve")
+
+### Partitioning ######## Partitioning ######## Partitioning ######## Partitioning ######## Partitioning #####
+### Partitioning ######## Partitioning ######## Partitioning ######## Partitioning ######## Partitioning #####
+### Partitioning ######## Partitioning ######## Partitioning ######## Partitioning ######## Partitioning #####
+### Partitioning ######## Partitioning ######## Partitioning ######## Partitioning ######## Partitioning #####
+
+#Basic recursive partitioning
+
+library(rpart)
+
+m2 <- rpart(good_bad_21~.,data=train)
+
+# Print tree detail
+printcp(m2)
+
+# Tree plot
+plot(m2, main="Tree:Recursive Partitioning");text(m2)
+#correlation matrix######################88888
+# Better version of plot
+prp(m2,type=2,extra=1,  main="Tree:Recursive Partitioning")
+
+# score test data
+test$m2_score <- predict(m2,type='prob',test)
+
+m2_pred <- prediction(test$m2_score[,2],test$good_bad_21)
+
+m2_perf <- performance(m2_pred,"tpr","fpr")
+
+# MOdel performance plot
+plot(m2_perf, lwd=2, colorize=TRUE, main="ROC m2: Traditional Recursive Partitioning")
+lines(x=c(0, 1), y=c(0, 1), col="red", lwd=1, lty=3);
+lines(x=c(1, 0), y=c(0, 1), col="green", lwd=1, lty=4)
+
+# Plot precision/recall curve
+m2_perf_precision <- performance(m2_pred, measure = "prec", x.measure = "rec")
+plot(m2_perf_precision, main="m2 Recursive Partitioning:Precision/recall curve")
+
+
+# Plot accuracy as function of threshold
+m2_perf_acc <- performance(m2_pred, measure = "acc")
+plot(m2_perf_acc, main="m2 Recursive Partitioning:Accuracy as function of threshold")
+
+# KS & AUC m1
+m2_AUROC <- round(performance(m2_pred, measure = "auc")@y.values[[1]]*100, 2)
+m2_KS <- round(max(attr(m2_perf,'y.values')[[1]]-attr(m2_perf,'x.values')[[1]])*100, 2)
+m2_Gini <- (2*m2_AUROC - 100)
+cat("AUROC: ",m2_AUROC,"\tKS: ", m2_KS, "\tGini:", m2_Gini, "\n")
+
+
+## Bayesian Partitioning
+
+
+# Fit Model 
+#### m2_1 <- rpart(good_bad_21~.,data=train_1,parms=list(prior=c(.9,.1)),cp=.0002) #- build model using 90%-10% priors
+# m2_1 <- rpart(good_bad_21~.,data=train_1,parms=list(prior=c(.8,.2)),cp=.0002) #- build model using 80%-20% priors
+####m2_1 <- rpart(good_bad_21~.,data=train,parms=list(prior=c(.7,.3)),cp=.0002)  #- build model using 70%-30% priors    ## Provide priors to a bayesian model  ## Notice, this are the correct priors
+#### m2_1 <- rpart(good_bad_21~.,data=train_1,parms=list(prior=c(.75,.25)),cp=.0002) #- build model using 75%-25% priors
+# m2_1 <- rpart(good_bad_21~.,data=train_1,parms=list(prior=c(.6,.4)),cp=.0002) #- build model using 60%-40% priors
+
+# Print tree detail
+printcp(m2_1)
+
+# plot trees
+plot(m2_1, main="m2_1-Recursive Partitioning - Using Bayesian N 70%-30%");text(m2_1)
+
+prp(m2_1,type=2,extra=1, main="m2_1-Recursive Partitioning - Using Bayesian N 70%-30%")
+
+test$m2_1_score <- predict(m2_1,type='prob',test)
+m2_1_pred <- prediction(test$m2_1_score[,2],test$good_bad_21)
+m2_1_perf <- performance(m2_1_pred,"tpr","fpr")
+
+# MOdel performance plot
+plot(m2_1_perf, colorize=TRUE, main="ROC m2_1: Recursive Partitioning - Using Bayesian N 70%-30%")
+lines(x=c(0, 1), y=c(0, 1), col="red", lwd=1, lty=3);
+lines(x=c(1, 0), y=c(0, 1), col="green", lwd=1, lty=4)
+
+#plots complexity vs. error
+plotcp(m2)
+plotcp(m2_1)
+
+# ROC Comparision
+plot(m2_perf,  colorize=TRUE, lwd=2,lty=1, main='Recursive partitioning:Model Performance Comparision (m2 ROC)') 
+plot(m2_1_perf, col='blue',lty=2, add=TRUE); # simple tree
+legend(0.4,0.4,
+			 c("m2: Traditional", "m2_1: Bayesian -70%-30%"),
+			 col=c('orange', 'blue'),
+			 lwd=3);
+lines(x=c(0, 1), y=c(0, 1), col="red", lwd=1, lty=3);
+lines(x=c(1, 0), y=c(0, 1), col="green", lwd=1, lty=4)
 
 
 
@@ -336,9 +437,6 @@ kfit_quanti<-kmeansvar( X.quanti = all_tweets_quanti, init = 5, iter.max = 150, 
 summary(kfit_quanti)
 plot(all_tweets_quanti, as.factor(kfit_quanti$cluster))
 kfit_quanti$E
-
-
-
 
 hist(all_tweets$follower_count)
 hist(all_tweets$following_count)
